65c3a38cbbd0b4312f36e01a16072e8104776aa3
wguo1 <parasiteneko@gmail.com>
2015-06-09 15:00:46 -0700

    changed auth again

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://<wguo1>:<123456>@apollo.modulusmongo.net:27017/ijo4Soxi' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://wguo1:123456@apollo.modulusmongo.net:27017/ijo4Soxi' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
