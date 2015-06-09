54e39cd58951ae3457ecb2e71da0107b5c1f4cf6
wguo1 <parasiteneko@gmail.com>
2015-06-09 14:53:45 -0700

    changed auth again

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://<wguo1>:<123456>@proximus.modulusmongo.net:27017/o8wugEre' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://wguo1:123456@proximus.modulusmongo.net:27017/o8wugEre' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
