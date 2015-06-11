2c7b9401664e0f754f1871dfaac6dadc74cd23c2
wguo1 <parasiteneko@gmail.com>
2015-06-10 20:33:42 -0700

    getting ideal weight

--- client/www/app/js/activityCtrl.js
+++ client/www/app/js/activityCtrl.js
@@ -5,68 +5,9 @@
         
      //needs refactoringggg!!!!!!!
      function activityCtrl($scope,$http,$location,appFactory){
-        $scope.activity_lv =['sedentary',
-                             'lightly active',
-                             'moderately active',
-                             'very active',
-                             'extra active'];
-
-        
-     
-        $scope.setActivity=function(item){
-            console.log(item);
-            appFactory.user.setActivity= item;
-           // $scope.calories=BMR+BMR*convertLevel();
-           // console.log($scope.calories);
-            
-            console.log(appFactory.user);
-            $location.path('/emailSignup');
-        };
-        // var calculateBMR = function(){
-        //     var weight=user.curr_weight;
-        //     var height=convertHeight(user.height);
-        //     var age=user.age;
-        //     if(user.gender==='female'){
-        //         return 655+4.3*user.weight+4.7*height-4.7*age;
-        //     }else if(user.gender==='male'){
-        //         return 655+6.3*user.weight+12.9*height-6.8*age;
-
-        //     }
-
-        // }
-        //convert the input height into inches
-        // var convertHeight = function(height){
-        //     var arr=height.split("'");
-        //     var foot =arr[0];
-        //     var inches =foot*12+arr[1];
-        //     return inches;
-        // }
-        // var convertLevel = function(){
-        //     var multiplier;
-        //     if(user.setActivity==="sedentary"){
-        //         multiplier=0.2;
-        //     }
-        //     else if(user.setActivity==="lightly active"){
-        //         multiplier=0.3;
-
-        //     }else if(user.setActivity==="moderately active"){
-        //         multiplier=0.4;
-
-        //     }else if(user.setActivity==="very active"){
-        //         multiplier=0.5;
-
-        //     }else if(user.setActivity==="extra active"){
-        //         multiplier=0.6;
-
-        //     }
-        //     return multiplier;
-        // }
-        // var BMR = calculateBMR();
-        // $scope.test = function(){
-            
-        // }
-        
-        appFactory.user.calories=$scope.calories;
+        console.log('this is the user',appFactory.user);
+        $scope.idealWeight = appFactory.user.idealWeight;
+        // $scope.idealWeight = 110;
     
      };
 
--- client/www/app/js/mainCtrl.js
+++ client/www/app/js/mainCtrl.js
@@ -1,21 +1,16 @@
 (function(){
     'use strict';
      angular.module('myApp.mainCtrl',[])
-     .controller('mainCtrl',['$scope','$http','$location', 'appFactory',mainCtrl]);
+     .controller('mainCtrl',['$scope','$rootScope','$http','$location', 'appFactory',mainCtrl]);
 
 
-    function mainCtrl($scope,$http,$location,appFactory){
+    function mainCtrl($scope,$rootScope,$http,$location,appFactory){
         console.log('inside mainCtrl');
         var user={};
-        $scope.showMenu = false;
-
-         //console.log(username);
-         //console.log('inside signup controllerrrrr');
-         $scope.goals = ['Lose Weight', 'Maintain Weight','Gain Weight'];
-         $scope.setGoal = function (item){
+        $scope.setGoal = function (item){
              appFactory.user.goal = item;
              $location.path('/genderandAge');
-         }
+         };
         // $scope.setName=function(item){
         //     appFactory.user.username=item;
         //     var user = appFactory.user;
@@ -29,22 +24,51 @@
         //         $location.path('/gender');
                 
         //  };
-        $scope.setMale = function(){
-            appFactory.user.gender='male';
-            $location.path('/age');
-        };
-        $scope.setFemale = function(){
-            console.log('called female');
-            appFactory.user.gender='female';
-            console.log(appFactory);
-        };
    
     
-        $scope.setGoalWeight = function(curr_weight, height_ft, height_in){
-            console.log('curr_weight is', curr_weight);
-            appFactory.user.height_ft = height_ft;
+        $scope.setGoalWeight = function(user){
+            //$scope.user = {};
+            console.log('inside of setGoalHeight');
+           
+            appFactory.user.birthday = user.birthday;
+            appFactory.user.curr_weight = user.curr_weight;
+            appFactory.user.height_ft = user.height_ft;
+            appFactory.user.height_inch = user.height_inch;
+            appFactory.user.gender = user.gender;
+           
+            // var birthdate = user.birthday.toString();
+           
+            var time2 = new Date().getTime();
+            var time1 = user.birthday.getTime();
+            var age = (time2-time1)/(1000*24*3600*356);
+            var weight;
+            appFactory.user.age = age;
+            console.log('height  in foot',user.height_ft);
+            console.log('height  in inch',user.height_inch);
+            if(user.gender==="female"){
+                if(user.height_ft<5){
+                    //JD Robinson's algorithm for ideal body weight
+                    weight= 52-1.92(60-parseInt(user.height_ft)*12-parseInt(user.height_inch));
+                    
+                }else{
+                    weight= 52+1.92*(parseInt(user.height_ft)*12+parseInt(user.height_inch)-60);
+                    console.log('the inches',parseInt(user.height_ft)*12+parseInt(user.height_inch)-60);
+
+                }
+                
+            }
+            
+            console.log('this is the user before redirect', appFactory.user)
+            console.log('weight is',weight);
+            appFactory.user.idealWeight = Math.floor(weight*2.20462);
+            console.log('calculated weight', appFactory.user.idealWeight);
+            //$rootScope.idealWeight = appFactory.user.weight;
+           
+           
+
+            //appFactory.user.height_ft = height_ft;
            // appFactory.user.curr_weight = weight;
-            //$location.path('/activity');
+           $location.path('/activity');
         };
         $scope.setAge = function(item){
             appFactory.user.age = item;
@@ -55,14 +79,14 @@
             appFactory.user.goal = goal;
             appFactory.user.goalDate = date; 
             $location.path('/stats');
-        }
+        };
         // $scope.setGoalWeight = function(){
         //     console.log('inside going to activity');
         //     $location.path('/activity');
         // }
         $scope.setCalorieBudget = function(){
             $location.path('/CalorieBudget');
-        }
+        };
         $scope.signupEmail = function(){
             console.log('signing up with email');
             $location.path('/goal');
